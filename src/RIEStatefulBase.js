3c6c4f0a838203030b020d52fc68985f385cc2c8
Martin Heidegger <martin.heidegger@gmail.com>
2018-06-25 14:02:13 +0900

    Details that actually were missing in the merge commit.

--- src/RIEStatefulBase.js
+++ src/RIEStatefulBase.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import RIEBase from "./RIEBase";
 
 const debug = require("debug")("RIEStatefulBase");
